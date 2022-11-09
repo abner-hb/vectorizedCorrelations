e25f3157765a31d36a27f1b9745a70fd2c0e44fc
Abner <113459290+abner-hb@users.noreply.github.com>
2022-11-09 12:26:28 -0500

    matrix_kendall now takes matrices whose columns are vectors

--- R/matrix_kendall.R
+++ R/matrix_kendall.R
@@ -1,11 +1,16 @@
 matrix_kendall = function(X, Y){
+    # Transpose so I can use row operations
+    X = t(X)
+    Y = t(Y)
     conc_pairs = rep(0L, nrow(X))
-    for (i in 1:(ncol(X) - 1)) {
+    for (i in 2:ncol(X)) {
         conc_pairs = conc_pairs +
             # Both entries are smaller
-            rowSums(X[, i] < X[, i:ncol(X)]) * rowSums(Y[, i] < Y[, i:ncol(X)]) +
+            rowSums(X[, i - 1] < X[, i:ncol(X), drop=FALSE])*
+            rowSums(Y[, i - 1] < Y[, i:ncol(X), drop=FALSE]) +
             # Both entries are bigger
-            rowSums(X[, i] > X[, i:ncol(Y)]) * rowSums(Y[, i] > Y[, i:ncol(Y)])
+            rowSums(X[, i - 1] > X[, i:ncol(Y), drop=FALSE])*
+            rowSums(Y[, i - 1] > Y[, i:ncol(Y), drop=FALSE])
     }
     tau_vec = 4*conc_pairs / (ncol(X)*(ncol(X) - 1)) - 1
     return(tau_vec)
--- R/test_matrix_kendall.R
+++ R/test_matrix_kendall.R
@@ -1,24 +1,31 @@
-X = matrix (c(1, 2, 2, 1), 2, 2)
-Y = t(matrix(1:nrow(X), nrow=nrow(X), ncol=ncol(X)))
+X = t(matrix (c(1, 2, 2, 1), 2, 2))
+Y = matrix(1:nrow(X), nrow=nrow(X), ncol=ncol(X))
+all.equal(
+    as.vector(cor(X, 1:nrow(X), method = "kendall")),
+    matrix_kendall(X, Y)
+)
+
+X = t(matrix (c(1, 2, 2, 1), 2, 2))
+Y = 1:nrow(X)
 all.equal(
     as.vector(cor(X, 1:nrow(X), method = "kendall")),
     matrix_kendall(X, Y)
 )
 
 
-X = matrix(c(2, 2, 1, 1), 2, 2)
-Y = t(matrix(1:nrow(X), nrow=nrow(X), ncol=ncol(X)))
+X = t(matrix(c(2, 2, 1, 1), 2, 2))
+Y = matrix(1:nrow(X), nrow=nrow(X), ncol=ncol(X))
 all.equal(
     as.vector(cor(X, 1:nrow(X), method = "kendall")),
     matrix_kendall(X, Y)
 )
 set.seed(854)
-test = matrix(rnorm(100000), nrow=1000, ncol=1000)
+test = t(matrix(rnorm(100000), nrow=1000, ncol=1000))
 matrix_kendall(test)
 microbenchmark::microbenchmark(matrix_kendall(test), times = 5)
 
 test_matrix_kendall = function(){
-    mtrx = matrix(rnorm(100000), nrow=1000, ncol=1000)
+    mtrx = t(matrix(rnorm(100000), nrow=1000, ncol=1000))
     n = 10
     max_tau = rep(NA_integer_, n)
     for (i in 1:n) {
