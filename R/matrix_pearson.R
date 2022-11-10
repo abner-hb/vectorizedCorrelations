5fa4fb4a9d9f9f6da9012485db1c67b5367467ab
Abner <113459290+abner-hb@users.noreply.github.com>
2022-11-10 16:19:41 -0500

    pearson now computes correlations; tests adjusted accordingly

--- R/matrix_pearson.R
+++ R/matrix_pearson.R
@@ -6,15 +6,22 @@ matrix_pearson = function(X, Y) {
         Y = matrix(Y, ncol=1)
     }
     n = nrow(X)
-    cov_mat = matrix(NA_real_, nrow=ncol(X), ncol=ncol(Y))
+    # Center matrices
     cntrd_X = t(t(X) - colMeans(X))
     cntrd_Y = t(t(Y) - colMeans(Y))
+    # Compute variances
+    Xstd_vec = sqrt(colSums(cntrd_X^2))
+    Ystd_vec = sqrt(colSums(cntrd_Y^2))
+    corr_mat = matrix(NA_real_, nrow=ncol(X), ncol=ncol(Y))
     for (i in 1:ncol(X)) {
-        product = cntrd_X[, i] * cntrd_Y
-        cov_mat[i,] = colSums(product) / (n - 1)
+        # Computing covariances
+        covariance_vec = cntrd_X[, i] * cntrd_Y
+        # Compute correlations
+        corr_mat[i,] = colSums(covariance_vec) / (Xstd_vec[i]*Ystd_vec)
     }
-    if (all(dim(cov_mat) == c(1,1))) {
-        cov_mat = cov_mat[1,1]
+    # Convert 1 by 1 matrix to number
+    if (all(dim(corr_mat) == c(1,1))) {
+        corr_mat = corr_mat[1,1]
     }
-    return(cov_mat)
+    return(corr_mat)
 }
--- tests/testthat/test-matrix_pearson.R
+++ tests/testthat/test-matrix_pearson.R
@@ -4,7 +4,7 @@ test_that("pearson works", {
     X = matrix(sample.int(100, size=50), nrow=10, ncol=5)
     Y = matrix(sample.int(100, size=50), nrow=10, ncol=5)
     expect_equal(
-        cov(X, Y),
+        cor(X, Y),
         matrix_pearson(X, Y)
     )
     # Case 2
@@ -12,7 +12,7 @@ test_that("pearson works", {
     X = matrix(rnorm(100*10, mean = 30, sd = 0.001), nrow=100, ncol=10)
     Y = matrix(rnorm(100*10, mean = 30, sd = 0.001), nrow=100, ncol=10)
     expect_equal(
-        cov(X, Y),
+        cor(X, Y),
         matrix_pearson(X, Y)
     )
     # Case 3
@@ -20,7 +20,7 @@ test_that("pearson works", {
     X = matrix(rnorm(100000), nrow=1000, ncol=1000)
     Y = matrix(rnorm(100000), nrow=1000, ncol=1000)
     expect_equal(
-        cov(X, Y),
+        cor(X, Y),
         matrix_pearson(X, Y)
     )
     # Case 4
@@ -28,7 +28,7 @@ test_that("pearson works", {
     X = matrix(rnorm(1000*100), nrow=1000, ncol=100)
     Y = matrix(rnorm(1000*1000), nrow=1000, ncol=1000)
     expect_equal(
-        cov(X, Y),
+        cor(X, Y),
         matrix_pearson(X, Y)
     )
     # Case 5
@@ -36,7 +36,7 @@ test_that("pearson works", {
     X = matrix(rnorm(1000*100), nrow=1000, ncol=100)
     Y = rcauchy(1000)
     expect_equal(
-        cov(X, Y),
+        cor(X, Y),
         matrix_pearson(X, Y)
     )
     # Case 6
@@ -44,7 +44,7 @@ test_that("pearson works", {
     X = rcauchy(1000)
     Y = matrix(rnorm(1000*100), nrow=1000, ncol=100)
     expect_equal(
-        cov(X, Y),
+        cor(X, Y),
         matrix_pearson(X, Y)
     )
     # Case 6
@@ -52,7 +52,7 @@ test_that("pearson works", {
     X = rcauchy(1000)
     Y = rcauchy(1000)
     expect_equal(
-        cov(X, Y),
+        cor(X, Y),
         matrix_pearson(X, Y)
     )
     # Case 7
@@ -60,33 +60,33 @@ test_that("pearson works", {
     X = matrix(rgamma(73*5000, shape = 10), ncol = 73, nrow=5000)
     Y = X + sample.int(100, size=73*5000, replace = TRUE)
     expect_equal(
-        cov(X, Y),
+        cor(X, Y),
         matrix_pearson(X, Y)
     )
 })
 
-# test_matrix_pearson = function(reps){
-#     X = matrix(rnorm(100000), nrow=1000, ncol=1000)
-#     Y = matrix(rnorm(100000), nrow=1000, ncol=1000)
-#     m = reps
-#     result = matrix_pearson(X, Y)
-#     for (i in 1:(m-1)) {
-#         result[] = matrix_pearson(X, Y)
-#     }
-#     return(result)
-# }
-# set.seed(2004)
-# profvis::profvis(test_matrix_pearson(10))
+test_matrix_pearson = function(reps){
+    X = matrix(rnorm(1000*500), nrow=1000, ncol=500)
+    Y = matrix(rnorm(1000*500), nrow=1000, ncol=500)
+    m = reps
+    result = matrix_pearson(X, Y)
+    for (i in 1:(m-1)) {
+        result[] = matrix_pearson(X, Y)
+    }
+    return(result)
+}
+set.seed(2004)
+profvis::profvis(test_matrix_pearson(10))
 #
-# test_r_pearson = function(reps){
-#     X = matrix(rnorm(100000), nrow=1000, ncol=1000)
-#     Y = matrix(rnorm(100000), nrow=1000, ncol=1000)
-#     m = reps
-#     result = cov(X, Y)
-#     for (i in 1:(m-1)) {
-#         result[] = cov(X, Y)
-#     }
-#     return(result)
-# }
-# set.seed(2004)
-# profvis::profvis(test_r_pearson(10))
+test_r_pearson = function(reps){
+    X = matrix(rnorm(1000*500), nrow=1000, ncol=500)
+    Y = matrix(rnorm(1000*500), nrow=1000, ncol=500)
+    m = reps
+    result = cor(X, Y)
+    for (i in 1:(m-1)) {
+        result[] = cor(X, Y)
+    }
+    return(result)
+}
+set.seed(2004)
+profvis::profvis(test_r_pearson(10))
